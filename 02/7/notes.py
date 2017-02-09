9ce8a8400e6c25d5996746495c6af2cc377fc134
david browning <dmbrowning21@gmail.com>
2017-02-09 09:44:53 -0700

    reformat so as to use date

---
+++ 02/7/notes.py
@@ -0,0 +1,18 @@
+# it is not the type of the object that matters, but what it can do
+## duck typing
+
+############################################################################################################################
+# Duck typingi increases the cognitive load on the programmer ( you have to know the type as a programmer )
+# Duck typing make project planning more difficult because it makes cases only project managers need to know the big
+#  pictures
+# Duck typing increases/complicates software testing
+#
+#
+############################################################################################################################
+
+
+############################################################################################################################
+# use dot operator to access class variables
+#
+#
+############################################################################################################################
