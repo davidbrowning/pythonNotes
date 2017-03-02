256ba6ff21f723102ae8e4944f652b49bc8acd02
david browning <dmbrowning21@gmail.com>
2017-03-02 10:14:36 -0700

    Updated notes

---
+++ 02/28/notes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+
+############################################################################################################################
+# I was messing around with mobaxterm instead of paying attention in class. Apparently we talked
+# a lot about image processing. Go look at the slides or something.
+#
+#
+############################################################################################################################
---
+++ 03/02/notes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+############################################################################################################################
+# Mean Median and Mode (three M's of statistics)
+# Median: odd is straighforward, even take mean of center 2 values
+#
+#
+#
+############################################################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
