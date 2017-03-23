fb291da2f240e07de9f7e834e8d3b284bed5e07c
david browning <dmbrowning21@gmail.com>
2017-03-23 10:10:48 -0600

    Updated notes

---
+++ 03/23/machineLearning.py
@@ -0,0 +1,31 @@
+############################################################################################################################
+# (ML)
+# -NumPy
+# -SciPy
+# -Matplotlib
+# Probability Density Functions and Distributions
+#
+# Branch of AI that investigates algorithms that learn from data.
+# Two broad categories: supervised and unsupervised.
+# Supervised(categorized): spam/no spam, hand written images, images with happy/angry faces
+#
+# Unsupervised(uncategorized): Large volumes of text docs that must be clustered into categories according to criteria.
+#
+############################################################################################################################
+
+############################################################################################################################
+#  Typical WF: Read&clean data: real world data always have typos, errors, missing values etc
+# Explore data: compute properties, visualize
+# Choose/develop algo to run it on data
+# Measure performance
+# publish results.
+#
+#
+############################################################################################################################
+
+############################################################################################################################
+# 3 Types of data
+# Numerical, Categorical, Ordinal
+#
+#
+############################################################################################################################
