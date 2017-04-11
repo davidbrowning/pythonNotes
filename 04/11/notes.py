9923ceeb6dccfa89a1073eefb9dacc7623333255
david browning <dmbrowning21@gmail.com>
2017-04-11 10:09:06 -0600

    Updated notes

---
+++ 04/11/notes.py
@@ -0,0 +1,13 @@
+############################################################################################################################
+#  Decision Tree learning: method for classifying discrete valued data
+#
+# Produce decision tree that produces a bunch of rules.
+#
+#  Lots of literature on the subject. 4 typical kinds of problems:
+#   1) classification of patient by symptom
+#   2) classify equipment malfunction by description
+#   3) classify load application by likelihood of defaulting
+#   4) classify resumes by likelihood of being hired.
+#
+#
+############################################################################################################################
